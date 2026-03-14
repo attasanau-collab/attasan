17a591a75dacc5ffae71eaf4ba1b072ef5c17d77
attasanau-collab <attasan.au@ksu.ac.th>
2026-03-13 22:54:38 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1249,7 +1249,7 @@ async function sendAI(){
 
   try{
 
-    const res = await fetch("/api/ai")",{
+    const res = await fetch("/api/ai",{
 
       method:"POST",
 
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
+
+app.use(express.static(__dirname));
 
 app.use(require("cors")());
 app.use(express.json());
